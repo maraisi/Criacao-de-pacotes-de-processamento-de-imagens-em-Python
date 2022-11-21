b52c38e1c86b5fc1d95b5fbd4672ccadd3cbbf4d
Maraisi de Paula Miranda <maraisi@debian-BULLSEYE-live-builder-AMD64>
2022-11-21 12:34:42 -0300

    README, requiriments, setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,10 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image_processing",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Maraísi",
+    description="Image Processing Package using Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
     url="my_github_repository_project_link"
